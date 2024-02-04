33d21d26b34f4183b2271e9f8f384f0cfb2c3faa
ashe-616 <ashe616@yahoo.com>
2024-02-04 16:48:00 -0600

    carousel effect

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Carousel from './components/Carousel/Carousel';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Carousel />
     </div>
   );
 }
