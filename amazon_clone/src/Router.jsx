22fde6fdeb54849d0a4a3052c419f9be55309795
ashe-616 <ashe616@yahoo.com>
2024-02-14 21:40:43 -0600

    payment ui

--- amazon_clone/src/Router.jsx
+++ amazon_clone/src/Router.jsx
@@ -6,7 +6,13 @@ import Payment from "./Pages/Payment/Payment";
 import Orders from "./Pages/Order/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
-import ProductDetail from './Pages/ProductDetail/ProductDetail';
+import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51OjVx6J6z6Ej8ZyBQer8fu4asRSdIVKdPhIKCCnCjZ5AF6SGu6ibj6FVqMdvN3lAIsF9XdtaoeshVpqh4BXgYOdd00xZNrDtJM"
+);
 
 function Routing() {
   return (
@@ -15,12 +21,18 @@ function Routing() {
         <Route>
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/payments" element={<Payment />} />
+          <Route
+            path="/payments"
+            element={
+              <Elements stripe={stripePromise}>
+                <Payment />
+              </Elements>
+            }
+          />
           <Route path="/orders" element={<Orders />} />
           <Route path="/category/:categoryName" element={<Results />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
-
         </Route>
       </Routes>
     </Router>
--- amazon_clone/src/components/DataProvider/DataProvider.jsx
+++ amazon_clone/src/components/DataProvider/DataProvider.jsx
@@ -1,14 +1,11 @@
+import React, { createContext, useReducer } from "react";
 
+export const DataContext = createContext();
 
-import React, {createContext, useReducer} from "react";
-
-
-export const DataContext = createContext()
-
-export const DataProvider = ({children, reducer, initialState}) => {
-    return(
-        <DataContext.Provider value={useReducer(reducer, initialState)}>
-            {children}
-        </DataContext.Provider>
-    )
-}
+export const DataProvider = ({ children, reducer, initialState }) => {
+  return (
+    <DataContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+    </DataContext.Provider>
+  );
+};
